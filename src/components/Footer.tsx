0148a88f2496cef02e7c00e6619636a71b32e2ec
Batuhan TANIR <batuhan.tanir00@gmail.com>
2025-12-17 04:13:11 +0300

    feat: enhance Footer component to utilize siteConfig for dynamic content and add social links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import { siteConfig } from "@/config/site";
+import React from "react";
 
-const Footer:React.FC = () => {
+const Footer: React.FC = () => {
   return (
-    <footer>
-      <div className="w-full bg-default-900 text-default-400 py-6 px-4 text-center text-small">
-        &copy; {new Date().getFullYear()} Pickle. All rights reserved.
+    <footer className="w-full py-16 bg-inherit flex flex-col items-center justify-center gap-4">
+      <p className="font-bold text-inherit uppercase">{siteConfig.name}</p>
+      <div className="flex gap-4">
+        {siteConfig.socialLinks.map((link, index) => (
+          <a
+            key={index}
+            href={link.href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-inherit border border-black dark:border-white rounded-full text-center p-1 flex items-center justify-center hover:opacity-90 active:opacity-80 active:scale-95 "
+          >
+            <link.Icon />
+          </a>
+        ))}
       </div>
+      <p className="text-sm text-inherit">
+        &copy; {new Date().getFullYear()} {siteConfig.name}. All rights
+        reserved.
+      </p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
